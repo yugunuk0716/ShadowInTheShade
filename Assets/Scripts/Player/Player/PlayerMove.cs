af8ffc07888a8cef74789075f1a0811faf219ee3
Eukkbi <smbaekr56@naver.com>
2022-01-23 19:34:46 +0900

    폴더 정리 및 뻘짓

--- Assets/Scripts/Player/Player/PlayerMove.cs
+++ Assets/Scripts/Player/Player/PlayerMove.cs
@@ -8,6 +8,9 @@ public class PlayerMove : AgentMove
 {
     private PlayerInput playerInput;
     private bool isDashing = false;
+    private Vector2 mousePos;
+    private Vector2 playerMousePos;
+    private float dashAngle;
 
     private void Start()
     {
@@ -22,11 +25,6 @@ public class PlayerMove : AgentMove
             OnMove(playerInput.dir.normalized, speed);
     }
 
-    private void Update()
-    {
-       // LookMouse();
-    }
-
     public override void OnMove(Vector2 dir, float speed)
     {
         base.OnMove(dir, speed);
@@ -34,17 +32,12 @@ public class PlayerMove : AgentMove
     public void Dash()
     {
         StopNormalMoving();
+     /*   mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        playerMousePos = (mousePos - (Vector2)transform.position).normalized;
+        dashAngle = Vector2.Angle((Vector2)transform.position + Vector2.up, playerMousePos);
+        Debug.Log(dashAngle);*/
         OnMove(playerInput.dir.normalized, GameManager.Instance.currentPlayerSO.moveStats.DPD);
         StartCoroutine(CheckDashEnd());
-    }
-
-    public void LookMouse()
-    {
-        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
-        this.transform.rotation = Quaternion.RotateTowards(transform.rotation
-                                                            ,Quaternion.Euler(0,0,angle)
-                                                            ,Time.deltaTime * GameManager.Instance.currentPlayerSO.ectStats.LPS);
     }
 
     public void StopNormalMoving()
