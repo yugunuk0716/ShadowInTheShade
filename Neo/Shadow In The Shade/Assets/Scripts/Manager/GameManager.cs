1bd43d1c2b79b8b166d1d9464ff2bd8dc25fed16
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-03-03 12:40:44 +0900

    플레이어 SO 추가

--- Neo/Shadow In The Shade/Assets/Scripts/Manager/GameManager.cs
+++ Neo/Shadow In The Shade/Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,4 +20,29 @@ public class GameManager : MonoBehaviour
             return _instance;
         }
     }
+
+    public Transform _player;
+    [Range(0f, 1f)]
+    public float _timeScale = 1f;
+
+    public UnityEvent _onPlayerDash; //플레이어가 대쉬할 때 쓰는 이벤트
+    public UnityEvent _onPlayerAttack; //플레이어가 공격할 때 쓰는 이벤트
+    public UnityEvent _onPlayerChangeType; //플레이어가 자신의 상태를 바꿀 때 스는 이벤트
+
+    public PlayerSO _playerSO;
+
+
+    private void Awake()
+    {
+        init();
+    }
+
+
+    public void init()
+    {
+        _player.GetComponent<SpriteRenderer>().sprite = _playerSO.playerSprite;
+        _playerSO.playerStates = PlayerStates.Human;
+        _playerSO.canChangePlayerType = true;
+    }
+
 }
---
+++ Neo/Shadow In The Shade/Assets/Scripts/Scriptable Object Scripts/PlayerSO.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PlayerStates // 플레이어 상태
+{
+    Human,
+    Shadow
+}
+
+[System.Serializable]
+public struct PlayerMoveStats
+{
+    [Header("이동 속도")]
+    public float SPD;
+
+    [Header("대쉬 속도")]
+    public float DPD;
+
+    [Header("대쉬 쿨타임")]
+    public float DCT;
+
+    [Header("대쉬 시간")]
+    public float DRT;
+
+    [Header("대쉬 스택")]
+    public int DSS;
+}
+
+
+[System.Serializable]
+public struct PlayerAttackStats
+{
+    [Header("공격력")]
+    public float ATK;
+
+    [Header("공격속도")]
+    public float ASD;
+
+    [Header("크리티컬 확률")]
+    public float CTP;
+    
+    [Header("크리티컬 데미지")]
+    public float CTD;
+
+    [Header("스킬 쿨타임")]
+    public float SCD;
+}
+
+[System.Serializable]
+public struct PlayerECTStats
+{
+    [Header("타입 변경 시간")]
+    public float TCT;
+
+    [Header("플레이어 체력")]
+    public int PHP;
+
+}
+
+
+[CreateAssetMenu(menuName = "SO/Player/PlayerSO")]
+public class PlayerSO : ScriptableObject
+{
+    [Header("플레이어 스프라이트")]
+    public Sprite playerSprite;
+
+    [Header("플레이어 현제 상태")]
+    public bool canChangePlayerType;
+    public PlayerStates playerStates;
+
+    [Header("플레이어의 이동스탯")]
+    public PlayerMoveStats moveStats;
+
+    
+    [Header("플레이어의 공격스탯")]
+    public PlayerAttackStats attackStats;
+
+    [Header("플레이어의 기타스탯")]
+    public PlayerECTStats ectStats;
+}
