3336a32ff1d0304d5aea4120473083b8a778be0d
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-06-09 19:38:03 +0900

    문 스프라이트 갈아끼움

---
+++ Neo/Shadow In The Shade/Assets/01.Scripts/Scriptable Object Scripts/DoorSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/DoorSO")]
+public class DoorSO : ScriptableObject
+{
+    public Sprite openedDoor;
+    public Sprite closedDoor;
+}
